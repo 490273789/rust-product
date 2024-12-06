f3528b9db0fca01d00e3f004a201b436e7552d40
王绍楠 <wangsn666@163.com>
2024-12-06 11:02:03 +0800

    feat: 添加cli相关文件

--- crates/csv_process/src/r_cli.rs
+++ crates/csv_process/src/r_cli.rs
@@ -1,6 +1,12 @@
-use std::path::Path;
-
 use clap::{command, Parser};
+use core::fmt;
+use std::{path::Path, str::FromStr};
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
 
 #[derive(Debug, Parser)]
 #[command(name = "r_cli", version, author, about, long_about = None)]
@@ -19,10 +25,16 @@ pub enum SubCommand {
 pub struct CsvOption {
     #[arg(short, long, value_parser=verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    pub output: String,
+
+    #[arg(short, long)] // "output.json".into()
+    pub output: Option<String>,
+
+    #[arg(long, value_parser = parse_format , default_value = "json")]
+    pub format: OutputFormat,
+
     #[arg(short, long, default_value_t = ',')] // default_value_t 不会做自动转换
     pub delimiter: char,
+
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
@@ -32,5 +44,35 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Invalid format!")),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
